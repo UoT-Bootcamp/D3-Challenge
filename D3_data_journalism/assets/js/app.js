ee61cab0ca8ab5b6826c511e3476b6628a0b80cc
preeti0103 <64617207+preeti0103@users.noreply.github.com>
2020-09-27 20:14:23 -0400

    Added code for first part

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,114 @@
-// @TODO: YOUR CODE HERE!
+// svg container
+var svgHeight = 500;
+var svgWidth = 700;
+
+// margins
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 80,
+    left: 100
+  };
+  
+  // chart area minus margins
+  var chartHeight = svgHeight - margin.top - margin.bottom;
+  var chartWidth = svgWidth - margin.left - margin.right;
+  
+  // Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+var svg = d3
+.select("#scatter")
+.append("svg")
+.attr("width", svgWidth)
+.attr("height", svgHeight);
+
+// Append an SVG group
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// var chosenXAxis = "poverty";
+
+// // function used for updating x-scale var upon click on axis label
+// function xScale(healthData, chosenXAxis) {
+//   // create scales
+//   var xLinearScale = d3.scaleLinear()
+//     .domain([d3.min(healthData, d => d[chosenXAxis]) * 0.8,
+//       d3.max(healthData, d => d[chosenXAxis]) * 1.2
+//     ])
+//     .range([0, width]);
+
+//   return xLinearScale;
+
+// }
+
+
+
+
+
+// Import Data
+d3.csv("assets/data/data.csv").then(function(healthData) {
+    
+    // console.log(healthData);
+
+    // Step 1: Parse Data/Cast as numbers
+    // ==============================
+    healthData.forEach(function(d) {
+      d.poverty = +d.poverty;
+      d.healthcare = +d.healthcare;
+    });
+
+
+    // Step 2: Create scale functions
+    // ==============================
+    var xLinearScale = d3.scaleLinear()
+      .domain([d3.min(healthData, d => d.poverty), d3.max(healthData, d => d.poverty)])
+      .range([0, chartWidth]);
+
+    var yLinearScale = d3.scaleLinear()
+      .domain([d3.min(healthData, d => d.healthcare), d3.max(healthData, d => d.healthcare)])
+      .range([chartHeight, 0]);
+
+    // Step 3: Create axis functions
+    // ==============================
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // Step 4: Append Axes to the chart
+    // ==============================
+    chartGroup.append("g")
+      .attr("transform", `translate(0, ${chartHeight})`)
+      .call(bottomAxis);
+
+    chartGroup.append("g")
+      .call(leftAxis);
+
+    // Step 5: Create Circles
+    // ==============================
+    var circlesGroup = chartGroup.selectAll("circle")
+    .data(healthData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("r", "10")
+    .attr("fill", "lightblue")
+    // .attr("opacity", "1");
+    
+
+    // Create axes labels
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left + 40)
+      .attr("x", 0 - (svgHeight / 2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Lacks Healthcare (%)");
+
+      chartGroup.append("text")
+      .attr("transform", `translate(${chartWidth / 3}, ${chartHeight + margin.top + 30})`)
+      .attr("class", "axisText")
+      .text("Hair Metal Band Hair Length (inches)");
+  }).catch(function(error) {
+    console.log(error);
+
+});
